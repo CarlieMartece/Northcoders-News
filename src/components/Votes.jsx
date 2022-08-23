2eebf71c6d0f6f1f3b7d934335def7f174bb4f0e
CarlieMartece <carliemartece@gmail.com>
2022-08-23 15:58:17 +0100

    show optimistic change in votes

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -1,24 +1,29 @@
 import { patchVotes } from "../api";
+import { useState } from "react";
 
 export default function Votes ({article_id, votes}) {
+
+    const [optimisticVote, setOptimisticVote] = useState(0);
 
     let voteCount = 0
     const incrementVote = () => {
         voteCount++;
-        patchVotes (article_id, voteCount).then((newVotes)=>{
-            console.log(newVotes)
+        patchVotes (article_id, voteCount);
+        setOptimisticVote((currOptimisticVote) => {
+            return currOptimisticVote + 1;
         })
     }
     const decreaseVote = () => {
         voteCount--;
-        patchVotes (article_id, voteCount).then((newVotes)=>{
-            console.log(newVotes)
+        patchVotes (article_id, voteCount);
+        setOptimisticVote((currOptimisticVote) => {
+            return currOptimisticVote - 1;
         })
     }
 
     return (
         <div className="article__votes">
-            <h4 className="article__votes-count">Votes: {votes}</h4>
+            <h4 className="article__votes-count">Votes: {votes + optimisticVote}</h4>
             <button 
                 className="article__votes-upvote"
                 onClick={incrementVote}
